a58e8e916d1d1cbe3f69dc24b0a1a2b2e8b33913
Tadeo Kondrak <hello@tdeo.me>
2015-04-30 10:16:43 -0600

    Update tipuesearch_content.js

--- tipuesearch/tipuesearch_content.js
+++ tipuesearch/tipuesearch_content.js
@@ -1,5 +1,5 @@
 var tipuesearch = {"pages": [
-     {"title": "Gold Miner", "text": "", "tags": "gold miner game gold diamonds dynamite", "url": "http://www.tipue.com"},
+     {"title": "Gold Miner", "text": "In Gold Miner, you have to direct your little crank to haul in gold nuggets. Every stage will have a cash goal that you must achieve. If the gold you've collected adds up to your goal then you move on to the next stage. Gold Miner offers a lot of variety in its gameplay though. There will be numerous sizes of gold in the game to reel in. Naturally, the large gold pieces will take forever and a day to haul but will worth much more cash in the end while the smaller pieces will fly towards you at light speed but be worth very little. Still, the trade off is fair and worth it. A single large gold nugget can often win the whole stage for you so go for the big ones first if you can. While hauling, beware of the ore pieces and especially the large ones because are worth squat and will take up time. The mystery bags are crazy and sometimes worth the risk since they haul in fast. Diamonds are the best though. Worth a lot of money but easy to haul in. Go for these right away at any cost.", "tags": "gold miner game gold diamonds dynamite", "url": "http://tadeokondrak.com/Arcadeacon/Games/Gold-Miner/"},
      ]};
 
 
