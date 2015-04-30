8e8579684fc6734dc5b834d627f1433d653dbd28
Tadeo Kondrak <hello@tdeo.me>
2015-04-30 10:27:18 -0600

    Update tipuesearch_content.js

--- tipuesearch/tipuesearch_content.js
+++ tipuesearch/tipuesearch_content.js
@@ -1,10 +1,20 @@
 var tipuesearch = {"pages": [
      {"title": "Gold Miner", "text": "", "tags": "gold miner game gold diamonds dynamite", "url": "http://arcadeacon.tdeo.me/Games/Gold-Miner/"},
      {"title": "Jacksmith", "text": "", "tags": "jacksmith blacksmith tools donkey ore cannon", "url": "http://arcadeacon.tdeo.me/Games/Jacksmith/"},
-     //{"title": "Gold Miner", "text": "", "tags": "gold miner game gold diamonds dynamite", "url": "http://arcadeacon.tdeo.me/Games/Gold-Miner/"},
-     //{"title": "Gold Miner", "text": "", "tags": "gold miner game gold diamonds dynamite", "url": "http://arcadeacon.tdeo.me/Games/Gold-Miner/"},
-     //{"title": "Gold Miner", "text": "", "tags": "gold miner game gold diamonds dynamite", "url": "http://arcadeacon.tdeo.me/Games/Gold-Miner/"},
-
+     {"title": "Papa Louie", "text": "", "tags": "papa louie", "url": "http://arcadeacon.tdeo.me/Games/Papa-Louie/"},
+     {"title": "Papa Louie 2", "text": "", "tags": "papa louie 2", "url": "http://arcadeacon.tdeo.me/Games/Papa-Louie-2/"},
+     {"title": "Papa Louie 3", "text": "", "tags": "papa louie 3", "url": "http://arcadeacon.tdeo.me/Games/Papa-Louie-3/"},
+     {"title": "Papa's Wingeria", "text": "", "tags": "papas papa's Wingeria", "url": "http://arcadeacon.tdeo.me/Games/Papas-Wingeria/"},
+     {"title": "Papa's Cupcakeria", "text": "", "tags": "papas papa's Cupcakeria", "url": "http://arcadeacon.tdeo.me/Games/Papas-Cupcakeria/"},
+     {"title": "Papa's Donuteria", "text": "", "tags": "papas papa's Donuteria", "url": "http://arcadeacon.tdeo.me/Games/Papas-Donuteria/"},
+     {"title": "Papa's Pastaria", "text": "", "tags": "papas papa's Pastaria", "url": "http://arcadeacon.tdeo.me/Games/Papas-Pastaria/"},
+     {"title": "Papa's Pizzeria", "text": "", "tags": "papas papa's Pizzeria", "url": "http://arcadeacon.tdeo.me/Games/Papas-Pizzeria/"},
+     {"title": "Papa's Freezeria", "text": "", "tags": "papas papa's Freezeria", "url": "http://arcadeacon.tdeo.me/Games/Papas-Freezeria/"},
+     {"title": "Papa's Pancakeria", "text": "", "tags": "papas papa's Pancakeria", "url": "http://arcadeacon.tdeo.me/Games/Papas-Pancakeria/"},
+     {"title": "Papa's Taco Mia", "text": "", "tags": "papas papa's Taco Mia", "url": "http://arcadeacon.tdeo.me/Games/Papas-Taco-Mia/"},
+     {"title": "Pappa's Burgeria", "text": "", "tags": "papas pappa's Burgeria", "url": "http://arcadeacon.tdeo.me/Games/Papa's Burgeria/"},
+     {"title": "Papa's Hot Doggeria", "text": "", "tags": "papas papa's Hot Doggeria", "url": "http://arcadeacon.tdeo.me/Games/Papas-Hot-Doggeria/"},
+     
 ]};
 
 
